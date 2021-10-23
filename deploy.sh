223ebe5a2f4124a63f7932738e98d666fee1112a
Taqi Raza <mttalpur@gmail.com>
2021-10-23 11:06:40 +0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -58,7 +58,7 @@ cp -Rp ./RedAlert1_Online "$WINEPREFIX"/drive_c/
 
 cp -Rp $WINEPREFIX ra-mp/ ; rm -rf $WINEPREFIX ; rm -rf ./ra-mp/winedata ; rm ./*.AppImage
 
-( cd ra-mp ; wget -qO- 'https://gist.github.com/mmtrt/6d111388fadf6a08b7f4c41cdc250080/raw/f8367c582eaf6e286c3844abf80beb168d028b73/cncraswp.patch' | patch -p1 )
+( cd ra-mp ; wget -qO- 'https://gist.github.com/mmtrt/6d111388fadf6a08b7f4c41cdc250080/raw/f322706a31415a66f34bcf110dd971728278bdf9/cncraswp.patch' | patch -p1 )
 
 export ARCH=x86_64; squashfs-root/AppRun -v ./ra-mp -n -u "gh-releases-zsync|mmtrt|cncra_AppImage|stable-wp|cncra*.AppImage.zsync" cncra_WP-${ARCH}.AppImage &>/dev/null
 
